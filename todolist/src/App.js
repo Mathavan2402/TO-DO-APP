bc3b41b010af6679cc2abd74b83234beb0498be7
Mathavan2402 <vigneshmathavan72@gmail.com>
2023-04-21 18:56:35 +0530

    maddy

---
+++ todolist/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <input>
+      <button>TODO</button>
+      </input>
+    </div>
+  );
+}
+
+export default App;
